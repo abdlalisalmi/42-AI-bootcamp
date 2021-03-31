7a4cd2aadc6234f189afc38967f63623d99a4168
abdelaali es salmi <abdlalisalmi@gmail.com>
2021-03-31 22:26:27 +0100

    finish module00/ex07

---
+++ python/module00/ex07/filterwords.py
@@ -0,0 +1,20 @@
+import sys
+from string import punctuation
+
+
+def filter_words(string, n):
+    words = [(word) for word in string.split(' ') if len(word) > int(n) ]
+    result = []
+    for word in words:
+        for c in punctuation:
+            word = word.replace(c, "")
+        result.append(word.replace(c, ""))
+    print(result)
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if len(args) != 2 or not args[1].isdecimal() or args[0].isdecimal():
+        print("ERROR")
+    else:
+        filter_words(args[0], args[1])
