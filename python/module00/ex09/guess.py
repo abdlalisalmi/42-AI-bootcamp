7095aef5bf77301c39e2e69d800b52cb6459985d
abdelaali es salmi <abdlalisalmi@gmail.com>
2021-03-31 23:12:46 +0100

    finish module00/ex09

---
+++ python/module00/ex09/guess.py
@@ -0,0 +1,29 @@
+from random import randint
+
+
+
+
+def guess():
+    print("""This is an interactive guessing game!
+You have to enter a number between 1 and 99 to find out the secret number.
+Type 'exit' to end the game.
+Good luck!
+""")
+    secret_number = randint(1, 99)
+    attempts = 0
+    print(secret_number)
+    while True:
+        attempts += 1
+        print("What's your guess between 1 and 99?")
+        user = int(input(">> "))
+        if user > secret_number:
+            print("Too high!")
+        elif user < secret_number:
+            print("Too low!")
+        else:
+            print(f"Congratulations, you've got it!\nYou won in {attempts} attempts!")
+            break
+
+
+if __name__ == '__main__':
+    guess()
