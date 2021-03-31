3e041677082f694ef67da1863efdd35b49ef02e4
abdelaali es salmi <abdlalisalmi@gmail.com>
2021-03-31 21:58:41 +0100

    finish module00/ex06

--- python/module00/ex06/recipe.py
+++ python/module00/ex06/recipe.py
@@ -26,18 +26,71 @@ def interface():
 3: Print a recipe
 4: Print the cookbook
 5: Quit""")
-    user = int(input(">> "))
+    while True:
+        try:
+            user = int(input(">> "))
+            if not user in range(1, 6):
+                raise ValueError
+            else:
+                break
+        except ValueError:
+            print("This option does not exist, please type the corresponding number.\nTo exit, enter 5.")
     return user
 
 
 def add_recipe():
-    pass
+    recipe_name = input("Please enter the recipe name: ")
+    ingredients = input("Please enter the ingredients list (ingre1, ingre2, ...): ")
+    meal = input("Please enter the meal: ")
+    prep_time = input("Please enter the preparation time in minutes: ")
+
+    cookbook.update({
+        recipe_name: {
+            'ingredients': [ing.strip() for ing in ingredients.split(',')],
+            'meal': meal,
+            'prep_time': prep_time
+        }
+    })
+    print(f"The recipe {recipe_name} created successfully.")
+    main()
+
+
+def delete_recipe():
+    while True:
+        recipe_name = input("Please enter the recipe name you want to delete. Or Q to quit\n>> ")
+        if recipe_name.lower() == 'q':
+            main()
+        try:
+            print(f"Delete recipe {recipe_name}...")
+            cookbook.pop(recipe_name)
+            main()
+            break
+        except:
+            print(f"Recipe with name {recipe_name} doesn't exist!")
 
 
 def print_recipe():
     print("Please enter the recipe's name to get its details:")
     name = input(">> ")
-    print(f"Recipe for {name}:")
+    recipe = cookbook.get(name, None)
+    if recipe:
+        print(f"Recipe for {name}:")
+        print(f"Ingredients list: {recipe.get('ingredients')}")
+        print(f"To be eaten for {recipe.get('meal')}")
+        print(f"Takes {recipe.get('prep_time')} minutes of cooking")
+        main()
+    else:
+        print(f"Recipe with name {name} doesn't exist!")
+        print_recipe()
+
+
+def print_cookbook():
+    for recipe in cookbook.keys():
+        print(f"Recipe for {recipe}:")
+        print(f"\tIngredients list: {cookbook.get(recipe).get('ingredients')}")
+        print(f"\tTo be eaten for {cookbook.get(recipe).get('meal')}")
+        print(f"\tTakes {cookbook.get(recipe).get('prep_time')} minutes of cooking\n")
+    main()
 
 
 def main():
@@ -45,13 +98,13 @@ def main():
     if user == 1:
         add_recipe()
     elif user == 2:
-        pass
+        delete_recipe()
     elif user == 3:
         print_recipe()
     elif user == 4:
-        pass
+        print_cookbook()
     elif user == 5:
-        print("Cookbook closed.")
+        print("\nCookbook closed.")
         exit()
     else:
         print("This option does not exist, please type the corresponding number.\nTo exit, enter 5.")
