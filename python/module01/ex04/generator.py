29ec3ebca8de117e8e402d5166a0e960db2ce71f
abdelaali es salmi <abdlalisalmi@gmail.com>
2021-04-02 21:05:06 +0100

    finish module01/ex04

--- python/module01/ex04/generator.py
+++ python/module01/ex04/generator.py
@@ -1,12 +1,10 @@
-
 from random import randint
-
 
 
 def generator(text, sep, option=None):
     '''Option is an optional arg, sep is mandatory'''
 
-    if not isinstance(text, str) or not option in ['shuffle', 'unique', 'ordered']:
+    if not isinstance(text, str) or not option in ['shuffle', 'unique', 'ordered', None]:
         return ["ERROR"]
 
     text = text.split(sep)
@@ -24,19 +22,22 @@ def generator(text, sep, option=None):
         text = result
 
     elif option == 'ordered':
-        pass
+        while True:
+            sort = False
+            for i in range(len(text) - 1):
+                if text[i] > text[i + 1]:
+                    text[i], text[i + 1] = text[i + 1], text[i]
+                    sort = True
+            if not sort:
+                break
             
-
     return text
 
 
 
 
 
-
-
-
-
-text = "Le Lorem Ipsum est simplement du faux texte."
-for word in generator(text, sep=" ", option="shuffle"):
-    print(word)
+if __name__ == '__main__':
+    text = "Le Lorem Ipsum est simplement du faux texte."
+    for word in generator(text, sep=" ", option="ordered"):
+        print(word)
